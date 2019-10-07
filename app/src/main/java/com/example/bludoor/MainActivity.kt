89c362fb389b01e7ba390862eff1ec6b144b331e
shedition <shedition@users.noreply.github.com>
2019-10-07 14:41:04 -0700

    Commit on 10/7 at 2:40PM

---
+++ app/src/main/java/com/example/bludoor/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.bludoor
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mtoolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(mtoolbar)
+        supportActionBar?.title="Bludoor"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val id = item?.itemId
+        when(id){
+            R.id.pair ->
+                Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+            R.id.changepwd ->
+                Toast.makeText(this,item.title.toString(), Toast.LENGTH_LONG).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun openBtnClicked(view: View){
+        Toast.makeText(this,"Open | Close Button Clicked", Toast.LENGTH_LONG).show()
+    }
+}
