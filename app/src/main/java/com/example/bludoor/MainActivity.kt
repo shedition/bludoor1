baa9af65e3d839f44288def3384ee21aa33a2e9c
shedition <shedition@users.noreply.github.com>
2019-10-14 19:24:51 -0700

    see if changes get uploaded to github

--- app/src/main/java/com/example/bludoor/MainActivity.kt
+++ app/src/main/java/com/example/bludoor/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.bludoor
 
 
+import android.bluetooth.BluetoothA2dp
+import android.bluetooth.BluetoothAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var ba: BluetoothAdapter? = null
+    private var btDeviceArrayAdapter: ArrayAdapter<String>?= null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +37,8 @@ class MainActivity : AppCompatActivity() {
         when(id){
             R.id.pair ->
                 Toast.makeText(this, item.title.toString(), Toast.LENGTH_LONG).show()
+
+
             R.id.changepwd ->
                 Toast.makeText(this,item.title.toString(), Toast.LENGTH_LONG).show()
         }
